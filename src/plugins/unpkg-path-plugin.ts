ce4de3f5c13a99b510bd3c2d8ea70f682908ab1b
Tyler <tylershelton99@gmail.com>
2022-04-10 23:35:35 -0400

    Refactor the code to pass through the input from the index page into the plugin and use that text to dynamically change the output.

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,7 +6,7 @@ const fileCache = localforage.createInstance({
   name: 'filecache',
 });
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -38,11 +38,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              import React from 'react';
-              const message = require('nested-test-pkg');
-              console.log(message, React);
-            `,
+            contents: inputCode,
           };
         }
 
