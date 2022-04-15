dc8ae3936ebf5a8a5c1fbfec0da9bf64a8af165f
Tyler <tylershelton99@gmail.com>
2022-04-15 15:53:35 -0400

    Work on some styling and create a resizable component that will allow the user to resize the windows vertically and horizontally.  Fix a bug that was causing the preview window to not display the executed code.

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,28 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
 import bundle from '../bundler/index';
+import Resizable from './resizable';
 
 const CodeCell = () => {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 1100);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
 
   return (
-    <div>
-      <CodeEditor
-        initialValue='const a = 1;'
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
+    <Resizable direction='vertical'>
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
+        <Resizable direction='horizontal'>
+          <CodeEditor
+            initialValue='const a = 1;'
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
+        <Preview code={code} />
       </div>
-      <Preview code={code} />
-    </div>
+    </Resizable>
   );
 };
 
@@ -31,8 +39,9 @@ export default CodeCell;
 /*  TEST CODE
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-const testComponent = () => {
+const TestComponent = () => {
   return (
     <div>
       <h1>Hello World!</h1>
@@ -41,6 +50,6 @@ const testComponent = () => {
   );
 };
 
-testComponent();
+ReactDOM.render(<TestComponent />, document.querySelector('#root'));
 
 */
