3f47a26f98d3ba2bfb1bb3cb3b74062de9016dac
Tyler <tylershelton99@gmail.com>
2022-04-14 20:48:12 -0400

    Enable the ability to run the code into the iframe, including React components using ReactDom

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
@@ -35,14 +36,24 @@ const App = () => {
       },
     });
 
-    setCode(result.outputFiles[0].text);
-
-    try {
-      eval(result.outputFiles[0].text);
-    } catch (err) {
-      alert(err);
-    }
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+    <html>
+    <head>
+    </head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener('message', (event) => {
+          eval(event.data);
+        }, false);
+      </script>
+    </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -54,13 +65,26 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe srcDoc={html} />
+      <iframe ref={iframe} sandbox='allow-scripts allow-modals' srcDoc={html} />
     </div>
   );
 };
 
-const html = `
-<h1>Local HTML doc</h1>
-`;
-
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+/*  TEST CODE
+
+import React from 'react';
+
+const testComponent = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>This is a test paragraph</p>
+    </div>
+  );
+};
+
+testComponent();
+
+*/
