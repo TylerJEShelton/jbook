e67cd5a7a338882f9b3fc36d016aa7445ad847ae
Tyler <tylershelton99@gmail.com>
2022-04-15 17:01:41 -0400

    Add in some build time error handling and errors caused during asynchronous code.

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -5,13 +5,15 @@ import bundle from '../bundler/index';
 import Resizable from './resizable';
 
 const CodeCell = () => {
-  const [input, setInput] = useState('');
   const [code, setCode] = useState('');
+  const [err, setErr] = useState('');
+  const [input, setInput] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(async () => {
       const output = await bundle(input);
-      setCode(output);
+      setCode(output.code);
+      setErr(output.err);
     }, 1100);
 
     return () => {
@@ -28,7 +30,7 @@ const CodeCell = () => {
             onChange={(value) => setInput(value)}
           />
         </Resizable>
-        <Preview code={code} />
+        <Preview code={code} err={err} />
       </div>
     </Resizable>
   );
