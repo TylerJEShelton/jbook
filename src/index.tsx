f6197a6ebf71e2bbb1973d1e6162dcdb3d2a4bb2
Tyler <tylershelton99@gmail.com>
2022-04-16 11:24:37 -0400

    Add a new type for Direction in actions/index to eliminate potential manual entry issues down the road.  Add the logic for the first case in cellsReducer (Update_Cell)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
+import { Provider } from 'react-redux';
+import { store } from './state';
 
 // Convert react-dom render to React 18 syntax
 // import ReactDOM from 'react-dom';
@@ -10,10 +12,12 @@ const root = createRoot(container!);
 
 const App = () => {
   return (
-    <div>
-      {/* <CodeCell /> */}
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <CodeCell />
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
