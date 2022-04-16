4b6c071296d1aedc7f3e815bd3065efae956ec18
Tyler <tylershelton99@gmail.com>
2022-04-15 22:38:06 -0400

    Create new text-editor component that will allow the user to add markdown text on the page.  Install MDEditor which will enable a prebuilt MD editor for the browser.  Implement the ability to click on the MD to edit or click off to only display the MD text.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 
 // Convert react-dom render to React 18 syntax
 // import ReactDOM from 'react-dom';
@@ -10,7 +11,8 @@ const root = createRoot(container!);
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      {/* <CodeCell /> */}
+      <TextEditor />
     </div>
   );
 };
