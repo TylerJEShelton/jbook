7a0321f5146b90865ed46d4e11b0d1ee93df26a0
Tyler <tylershelton99@gmail.com>
2022-04-17 12:11:09 -0400

    Install fontawesome.  Add styling to the ActionBar buttons and add the icons to replace the words.

--- src/components/action-bar.tsx
+++ src/components/action-bar.tsx
@@ -1,16 +1,39 @@
+import './action-bar.css';
 import { useActions } from '../hooks/use-actions';
 
 interface ActionBarProps {
   id: string;
 }
 
+// Refactor the buttons into a separate ActionButtons component that will take in a onClick and icon
 const ActionBar: React.FC<ActionBarProps> = ({ id }) => {
   const { moveCell, deleteCell } = useActions();
   return (
-    <div>
-      <button onClick={() => moveCell(id, 'up')}>Up</button>
-      <button onClick={() => moveCell(id, 'down')}>Down</button>
-      <button onClick={() => deleteCell(id)}>Delete</button>
+    <div className='action-bar'>
+      <button
+        className='button is-primary is-small'
+        onClick={() => moveCell(id, 'up')}
+      >
+        <span className='icon'>
+          <i className='fas fa-arrow-up'></i>
+        </span>
+      </button>
+      <button
+        className='button is-primary is-small'
+        onClick={() => moveCell(id, 'down')}
+      >
+        <span className='icon'>
+          <i className='fas fa-arrow-down'></i>
+        </span>
+      </button>
+      <button
+        className='button is-primary is-small'
+        onClick={() => deleteCell(id)}
+      >
+        <span className='icon'>
+          <i className='fas fa-times'></i>
+        </span>
+      </button>
     </div>
   );
 };
